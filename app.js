5f5d272ec7259120a15877b2e5e0946dc8a089c7
Muhammadabbas12345 <abbasarbab007@gmail.com>
2022-03-28 17:09:24 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,415 @@
+
+let z= 20
+
+let b= 20
+console.log (z+b)
+
+let named ={
+    fn : "abbas",
+    ln: "ali",
+    age: 20
+}
+console.log ( typeof named .ln)
+
+let
+a=2
+b=3
+a++;
+console.log(a+b)
+
+let
+c=2
+
+c++;
+console.log(c)
+
+
+let
+d=2
+
+d--;
+console.log(d)
+
+let e =2
+let f = 2;
+let vv =
+e**f
+console.log(vv)
+
+
+
+
+
+
+
+
+let g = 8
+let h = 4
+
+console.log(g/h)
+
+
+let
+aa=2
+bb=3
+aa++;
+console.log(aa+bb)
+
+let
+cc=2
+
+cc++;
+console.log(cc)
+
+
+let
+dd=2
+
+dd--;
+console.log(dd)
+
+let ee =2
+let ff = 2;
+let vvvv =
+ee**ff
+console.log(vvvv)
+
+
+
+
+
+
+
+
+let gg = 8
+let hh = 4
+
+console.log(gg/hh)
+
+
+let
+aaa=2
+bbb=3
+aaa++;
+console.log(aaa+bbb)
+
+let
+ccc=2
+
+c++;
+console.log(ccc)
+
+
+let
+ddd=2
+
+ddd--;
+console.log(ddd)
+
+let eee =2
+let fff = 2;
+let vvcc =
+eee**fff
+console.log(vvcc)
+
+
+
+
+
+
+
+
+let ggg = 8
+let hhh = 4
+
+console.log(ggg/hhh)
+
+let
+aaaa=2
+bbbb=3
+aaaa++;
+console.log(aaaa+bbbb)
+
+let
+cccc=2
+
+cccc++;
+console.log(cccc)
+
+
+let
+dddd=2
+
+dddd--;
+console.log(dddd)
+
+let eeee =2
+let ffff = 2;
+let vvff =
+eeee**ffff
+console.log(vvff)
+
+
+
+
+
+
+
+
+let gggg = 8
+let hhhh = 4
+
+console.log(gggg/hhhh)
+
+let
+aaaaa=2
+bbbbb=3
+aaaaa++;
+console.log(aaaaa+bbbbb)
+
+let
+ccccc=2
+
+ccccc++;
+console.log(ccccc)
+
+
+let
+ddddd=2
+
+ddddd--;
+console.log(ddddd)
+
+let eeeee =2
+let fffff = 2;
+let vvdd =
+eeeee**fffff
+console.log(vvdd)
+
+
+
+
+
+
+
+
+let ggggg = 8
+let hhhhh = 4
+
+console.log(ggggg/hhhhh)
+
+
+let r = 24
+let s = 12
+let t = 14
+
+console.log(r/s*t+r-t)
+
+
+
+var k = 5;
+
+let l = k * 10;
+
+console.log(l)
+
+let name = "abbas"
+let age = 20;
+
+document.write(`my name is ${name} and iam a ${age} year old`)
+
+let names = "abbas"
+let ages = 20;
+
+console.log(`my name is ${names} and iam a ${ages} year old`)
+
+
+let 
+ student={
+    fn : "Muhammad",
+    ln : "Abbas",
+    semester : "3rd",
+    age : 19,
+
+
+    
+   
+
+}
+
+console.log(    `My name is ${student. fn} ${student.ln} I am read in ${student.semester} semester and i am ${student.age} year old`)
+
+let nam = " my name is  \"abbas\"  "
+console.log(nam)
+
+
+let nam1="muhammad";
+let nam2="abbas";
+let add= nam1.concat(" ", nam2)
+console.log(add)
+
+
+
+let lent="muhammad abbas arbab";
+let check= lent.length;
+console.log(check)
+
+
+var nam3="abbas";
+nam3.toUpperCase();
+
+console.log(nam3)
+
+
+
+
+
+
+let text="i am a good student";
+let upper=text.toUpperCase()
+console.log(upper)
+
+let text2="i am a good student";
+let lower=text.toLocaleLowerCase()
+console.log(lower)
+
+
+let text3= "gooder"
+let ans= text3.charAt(4)
+console.log(ans)
+
+let text4="      im a good students     ";
+let ans2= text4.trim();
+console.log(ans2)
+
+
+
+let text5={};
+text5.name="abbas"
+text5["age"]=20
+let prop="name"
+console.log(text5[prop])
+
+
+console.log(Math.ceil(1.1) )
+
+
+
+
+// upper case
+
+let na="abbas";
+let ans9=na.toUpperCase()
+console.log(ans9)
+// lower case
+let na1="ABBAS";
+let ans0= na1.toLocaleLowerCase()
+console.log(ans0)
+
+// charAt
+
+let na2="abbas";
+let ans5= na2.charAt(2)
+console.log(ans5)
+
+// trim
+
+let na3="          abbas        ";
+let ans6=na3.trim()
+console.log(ans6)
+
+// braket and dot
+
+let na4= {};
+na4.name2="abbas"
+na4["age"]=20
+let prop1= "name2"
+
+console.log(na4)
+
+// ceil
+
+console.log(Math.ceil(2.3))
+
+
+// salary
+
+let salary = [
+    ["ali", 12 ,  "50k" ],
+    ["saim", 12 ,  "70k" ],
+    ["rashid", 12 ,  "3k" ],
+    ["zeeshan", 12 ,  "5k" ],
+
+]
+
+
+console.log(salary)
+
+ let ani = [fnn = "cow", data={
+     nikname : "zeee",
+     age : 33,
+     class : 12
+ }]
+ console.log(ani,"cow data")
+//  arrey methods
+// 1) to string
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits, "sring")
+console.log(fruits.toString())
+// 2) join
+const fruitss = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruitss, "join")
+console.log(fruitss.join(" * "),"join")
+// 3) pop its remove the last element from arrey
+const fruitse = ["Banana", "Orange", "Apple", "Mango"];
+fruitse.pop()
+console.log(fruitse,"pop")
+console.log(fruitse.pop(),"pop")
+// 4> push it push new value to arrey
+const fruit = ["Banana", "Orange", "Apple", "Mango"];
+fruit.push("ali");
+console.log(fruit,"push")
+// 5) shift it  remove first element from  arrey
+const fru = ["Banana", "Orange", "Apple", "Mango"];
+fru.shift();
+console.log(fru,"shift" )
+// 6) unshift it add elememt in arrey at index 0
+const frui = ["Banana", "Orange", "Apple", "Mango"];
+frui.unshift("Lemon");
+console.log(frui,"unshift")
+// 7) concat  it murge all arreyss
+const array1 = ["Cecilie", "Lone"];
+const array2 = ["Emil", "Tobias", "Linus"];
+const array3 = ["Robin", "Morgan"];
+const myChildren = array1.concat(array2, array3);
+console.log(myChildren,"concat")
+// 8) splice  add new atimes at given index      to given exixting index
+const fruis = ["Banana", "Orange", "Apple", "Mango"];
+fruis.splice(2, 0, "Lemon", "Kiwi");
+console.log(fruis,"splice")
+// splpice also remove element from given point
+const fs = ["Banana", "Orange", "Apple", "Mango"];
+fs.splice(0,1)
+console.log(fs)
+
+// sort
+let numb =  [3,4,8,9,1]
+numb.sort()
+console.log(numb)
+
+// prompt
+let namee = 'abbas'
+
+prompt(namee)
+// alert
+
+let alrt= 'abbas'
+alert(alrt)
+
+
+// operator
+
+let ab =- 10
+let ba =- 10
+console.log(ab+ba)
+ 
